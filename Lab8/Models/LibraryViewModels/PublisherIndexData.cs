7bf8b98b25e3d721d7e42dc935a935a09e7597c3
DianaV <dianavirlan25@yahoo.com>
2020-11-29 15:56:31 +0200

    commit

---
+++ Lab8/Models/LibraryViewModels/PublisherIndexData.cs
@@ -0,0 +1,15 @@
+﻿using LibraryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Virlan_Diana_Lab2.Models.LibraryViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
